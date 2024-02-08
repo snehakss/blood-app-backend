c9242ebce8df328b9d0689b4c05060391650214d
snehakss <snehakallingal123@gmail.com>
2024-02-08 23:48:05 +0530

    search added

--- controller/bloodRouter.js
+++ controller/bloodRouter.js
@@ -16,4 +16,10 @@ router.get("/view",async(req,res)=>{
     let data=await bloodModel.find()
     res.json(data)
 })
+
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await bloodModel.find(input)
+    res.json(data)
+})
 module.exports=router
